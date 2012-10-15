776ae9cb04755761be182282da82ff6bcc51e934
Dylan Silva <dylan.msilva@gmail.com>
2012-10-14 21:51:11 -0700

    Added new modules and changes to triangle for week 3.

--- triangle.py
+++ triangle.py
@@ -1,3 +1,5 @@
+import math
+
 def area(base, height):
     '''(number, number) -> number
 
@@ -39,3 +41,18 @@ def semiperimeter(side1, side2, side3):
     '''
 
     return perimeter(side1, side2, side3) / 2
+
+def area_hero(side1, side2, side3):
+    ''' (number, number, number) -> float
+
+    Return the area of a triangle with sides of length
+    side1, side2, and side3.
+
+    >>> area_hero(3, 4, 5)
+    6.0
+    >>> area_hero(10.5, 6, 9.3)
+    27.73168584850189
+    '''
+    semi = semiperimeter(side1, side2, side3)
+    area = math.sqrt(semi * (semi -side1) * (semi - side2) * (semi - side3))
+    return area
